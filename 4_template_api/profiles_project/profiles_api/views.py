5a3c75cb1a5e8b6f25155868bda1a6999d4e3733
Chengyu Huang <chengyuhuang16@gmail.com>
2017-05-09 14:44:06 -0400

    add methods to our viewsets

--- 4_template_api/profiles_project/profiles_api/views.py
+++ 4_template_api/profiles_project/profiles_api/views.py
@@ -10,7 +10,7 @@ from . import serializers
 class HelloApiView(APIView):
     """Test API View"""
 
-    serializers_class = serializers.HelloSerializer
+    serializer_class = serializers.HelloSerializer
 
     def get(self,request,format=None):
         """Returns a list of APIView features."""
@@ -50,6 +50,7 @@ class HelloApiView(APIView):
 
 class HelloViewSet(viewsets.ViewSet):
     """Test API ViewSet """
+    serializer_class = serializers.HelloSerializer
 
     def list(self,request):
         """Return a Hello message. """
@@ -59,3 +60,34 @@ class HelloViewSet(viewsets.ViewSet):
             'Provides more functionality with less code'
         ]
         return Response({'message':'Hello','an_apiviewset':an_apiviewset})
+
+    def create(self,request):
+        """create a new hello message."""
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self,request,pk=None):
+        """Handles getting an object by its ID"""
+
+        return Response({'http_method':'GET'})
+
+    def update(self,request,pk=None):
+        """Handles updating an object by its ID"""
+
+        return Response({'http_method':'PUT'})
+
+    def partial_update(self,request,pk=None):
+        """Handles partial updating an object by its ID"""
+
+        return Response({'http_method':'PATCH'})
+
+    def destroy(self,request,pk=None):
+        """Handles removing an object by its ID"""
+
+        return Response({'http_method':'DELETE'})
